61b69e81a2670954d5b02979cc4d160652cb24d1
Hty <15161656368@163.com>
2025-09-23 21:54:45 +0800

    提交文件

--- Comment/src/main/java/com/hty/comment/service/IBlogService.java
+++ Comment/src/main/java/com/hty/comment/service/IBlogService.java
@@ -1,9 +1,13 @@
 package com.hty.comment.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hty.comment.dto.Result;
 import com.hty.comment.entity.Blog;
 
 
 public interface IBlogService extends IService<Blog> {
 
+    Result queryBlogById(Long id);
+
+    Result queryHotBlog(Integer current);
 }
--- Comment/src/main/java/com/hty/comment/service/impl/BlogServiceImpl.java
+++ Comment/src/main/java/com/hty/comment/service/impl/BlogServiceImpl.java
@@ -1,13 +1,55 @@
 package com.hty.comment.service.impl;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hty.comment.dto.Result;
 import com.hty.comment.entity.Blog;
+import com.hty.comment.entity.User;
 import com.hty.comment.mapper.BlogMapper;
 import com.hty.comment.service.IBlogService;
+import com.hty.comment.service.IUserService;
+import com.hty.comment.utils.SystemConstants;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 
 @Service("blogService")
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
+    
+    @Resource
+    private IUserService userService;
+    private void queryBlogUser(Blog blog) {
+        Long userId = blog.getUserId();
+        User user = userService.getById(userId);
+        blog.setName(user.getNickName());
+        blog.setIcon(user.getIcon());
+    }
+    @Override
+    public Result queryBlogById(Long id) {
+        // 查询blog
+        Blog blog = getById(id);
+        if (blog == null) {
+            return Result.fail("笔记不存在！");
+        }
+        // 查询blog有关的用户
+        queryBlogUser(blog);
+        return Result.ok(blog);
+    }
 
+
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> page = query()
+                .orderByDesc("liked")
+                .page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = page.getRecords();
+        // 查询用户
+        records.forEach(this::queryBlogUser);
+        return Result.ok(records);
+    }
 }
