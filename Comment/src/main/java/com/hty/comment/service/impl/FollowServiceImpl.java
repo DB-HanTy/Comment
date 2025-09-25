2a8cb94ed1663bb332945b5fb7e855bf6fa2bd45
Hty <15161656368@163.com>
2025-09-25 09:09:57 +0800

    提交文件

--- Comment/src/main/java/com/hty/comment/service/impl/FollowServiceImpl.java
+++ Comment/src/main/java/com/hty/comment/service/impl/FollowServiceImpl.java
@@ -1,23 +1,32 @@
 package com.hty.comment.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hty.comment.dto.Result;
+import com.hty.comment.dto.UserDTO;
 import com.hty.comment.entity.Follow;
 import com.hty.comment.mapper.FollowMapper;
 import com.hty.comment.service.IFollowService;
+import com.hty.comment.service.IUserService;
 import com.hty.comment.utils.UserHolder;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service("followService")
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IUserService userService;
     @Override
     public Result follow(Long followUserId, Boolean isFollow) {
         //1. 获取登录用户
@@ -54,5 +63,27 @@ public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> impleme
         Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
         //3. 判断
         return Result.ok(count > 0);
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        //1. 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        String key = "follows:" + userId;
+        //2. 求交集
+        String key2 = "follows:" + id;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(key,key2);
+        if (intersect == null || intersect.isEmpty() ){
+            //无交集
+            return Result.ok(Collections.emptyList());
+        }
+        //3. 解析id集合
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        //4. 查询用户
+        List<UserDTO> users = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(users);
     }
 }
